21b45b62accd1112ab754f2bcd074c8c336f7bab
lu-perez <luperez@teco.com.ar>
2023-04-27 14:51:37 -0300

    register ui

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -12,6 +12,9 @@ import {
 import useRegisterModal from '@/app/hooks/useRegisterModal'
 import Modal from './Modal'
 import Heading from '../Heading'
+import Input from '../inputs/Input'
+import { toast } from 'react-hot-toast'
+import Button from '../Button'
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal()
@@ -37,7 +40,7 @@ const RegisterModal = () => {
         registerModal.onClose()
       })
       .catch((error) => {
-        console.log(error)
+        toast.error('Something went wrong')
       })
       .finally(() => {
         setIsLoading(false)
@@ -50,6 +53,61 @@ const RegisterModal = () => {
         title='Welcome to Airbnb'
         subtitle='Create an Account!'
       />
+      <Input
+        id='email'
+        label='email'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id='name'
+        label='name'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id='password'
+        label='password'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className='flex flex-col gap-4 mt-3'>
+      <hr />
+      <Button
+        outline
+        label='Continue with Google'
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label='Continue with Github'
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className='text-neutral-500 text-center mt-4 font-light'>
+        <div className='justify-center flex flex-row items-center gap-2'>
+          <div>
+            Already have an account?
+          </div>
+          <div
+            onClick={registerModal.onClose}
+            className='text-neutral-800 cursor-pointer hover:underline'
+          >
+            Log in
+          </div>
+        </div>
+      </div>
     </div>
   )
 
@@ -62,6 +120,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
 import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <RegisterModal />
         <Navbar />
         {children}
